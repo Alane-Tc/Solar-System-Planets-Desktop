4a3e2cb655650d6a19b71c8a3cbdd45ee2713f64
alan-tc <alantamez28@hotmail.com>
2021-03-30 14:44:36 -0600

    earth

---
+++ src/public/js/earth.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", function () {
+    let button = document.querySelector("#btn-eath");
+    button.addEventListener("click", function () {
+        alert("Ya sabes tu edad en este planeta");
+    })
+
+})
